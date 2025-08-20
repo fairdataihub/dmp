5196875252317700b028fb1da2e2363263639d30
Xuebin Dong <xdong@Xuebin-MacBook-Pro.local>
2025-08-20 09:47:48 -0700

    Update last page

--- app.vue
+++ app.vue
@@ -14,11 +14,11 @@ useHead({
 useSeoMeta({
   title: "DMP Chef", // The page title
   ogImage:
-    "https://kalai.fairdataihub.org/api/generate?title=Hello&description=This%20is%20a%20Nuxt%20starter%20template&app=fairdataihub&org=fairdataihub",
+    "https://kalai.fairdataihub.org/api/generate?title=DMP%20Chef&description=A%20survey%20on%20AI-assisted%20Data%20Management%20Plans%20(DMPs).&app=fairdataihub&org=fairdataihub",
   titleTemplate: "%s",
   twitterCard: "summary_large_image",
   twitterImage:
-    "https://kalai.fairdataihub.org/api/generate?title=Hello&description=This%20is%20a%20Nuxt%20starter%20template&app=fairdataihub&org=fairdataihub",
+    "https://kalai.fairdataihub.org/api/generate?title=DMP%20Chef&description=A%20survey%20on%20AI-assisted%20Data%20Management%20Plans%20(DMPs).&app=fairdataihub&org=fairdataihub",
 });
 </script>
 
